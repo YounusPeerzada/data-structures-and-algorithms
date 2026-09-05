1c20afd184b1188d787489045d9accdf78326b6f
Muhammad Younus Peerzada <pakistan14612@gmail.com>
2026-09-05 21:25:42 +0500

    implement binary tree creation from preorder

---
+++ Binary Tree/binary_tree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+   public:
+    int data;
+    Node* left;
+    Node* right;
+
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int static idx = -1;
+
+Node* binaryTree(vector<int> preorder) {
+    idx++;
+
+    if (preorder[idx] == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(preorder[idx]);
+    root->left = binaryTree(preorder);
+    root->right = binaryTree(preorder);
+
+    return root;    
+}
+
+int main () {
+    vector<int> preorder = {1, 2, -1, -1,  3, -1, -1};
+
+    Node* root = binaryTree(preorder);
+
+    cout << root->data << endl;
+    cout << root->left->data << endl;
+    cout << root->right->data << endl;
+}
